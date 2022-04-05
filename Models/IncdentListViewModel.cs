ee27cc902ae0bb454a8681ca02e7ca06641b8a39
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-04-04 23:14:00 -0400

    started exploring Incidentviewmodel

---
+++ Models/IncdentListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace A1.Models
+{
+    public class IncdentListViewModel: IncidentViewModel
+    {
+        public List<Incident> Incidents { get; set; }
+
+
+    }
+}
---
+++ Models/IncidentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace A1.Models
+{
+    public class IncidentViewModel
+    {
+        public Incident incident { get; set; }
+        public Customer customer { get; set; }
+        public Product product { get; set; }
+        public Technician technician { get; set; }
+
+        public string operation { get; set; }
+
+    }
+}
