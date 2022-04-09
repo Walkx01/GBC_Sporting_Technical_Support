2f4eb22bfbd91c55e124717c05ca4d95ced89594
Noor Soreti <noor.said0698@gmail.com>
2022-04-08 23:18:30 -0400

    techIncident

--- Controllers/techIncident.cs
+++ Controllers/techIncident.cs
@@ -36,19 +36,25 @@ namespace A1.Controllers
             Incident? incident = context.Incidents.Include(i => i.customer)
                             .Include(i => i.product)
                             .Include(i => i.technician).Where(i => i.technicianID == id).FirstOrDefault();
-           
+            ViewBag.Product = context.Products.ToList();
+            ViewBag.Customer = context.Customers.ToList();
+            ViewBag.Technician = context.Technician.ToList();
+
             return View(incident);
         }
 
         [HttpPost]
         public IActionResult Edit(Incident incident)
         {
+            ViewBag.Product = context.Products.ToList();
+            ViewBag.Customer = context.Customers.ToList();
+            ViewBag.Technician = context.Technician.ToList();
             if (ModelState.IsValid)
             {
                 TempData["message"] = "Successfully Updated incident: " + incident.title;
                 context.Incidents.Update(incident);
                 context.SaveChanges();
-                return RedirectToAction("List");
+                return RedirectToAction("ListTechnicians");
             }
           
             return View(incident);
